f20abd723fdffae4fb2f9e206cdf5cf5342fed92
Subhadip Das <subhadipdas911@gmail.com>
2024-12-18 20:34:50 +0530

    Print in an Increasing sequence

---
+++ Topics/src/main/java/Recursion/PrintIncreasingManner.java
@@ -0,0 +1,27 @@
+package Recursion;
+
+/*
+ * Print in an increasing manner 1,2,3,4...
+ */
+
+public class PrintIncreasingManner {
+	
+	static String printIncreasing(int N) {
+		if (N == 1) {
+			String ans = "";
+			ans += "1";
+			ans += " ";
+			return ans;
+		}
+		String result = printIncreasing(N-1);
+		String ans =  result + N;
+		ans += " ";
+		return ans;
+	}
+	
+	public static void main(String[] args) {
+		String ans = printIncreasing(12);
+		System.out.println(ans);
+	}
+
+}
